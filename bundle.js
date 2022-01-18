45dae0a422ce1c8f50792294823c37f763ae7b5b
Gus <82774618+GooseterV@users.noreply.github.com>
2022-01-18 09:44:22 -0500

    g

--- bundle.js
+++ bundle.js
@@ -1,12 +1,14 @@
+let streak = 0;
+
 async function answerQuestion(button) {
 	const questionelement = document.getElementById("question");
 	const question = flashcards[questionelement.getAttribute("data-index")];
 	const answerIndex =  question.answers.indexOf(button.innerText);
 	const correctAnswerIndex = question.correct;
 	if (question.answers[answerIndex] === question.answers[correctAnswerIndex]) {
-		await answeredCorrectly()
+		await answeredCorrectly();
 	} else if (question.answers[answerIndex] !== question.answers[correctAnswerIndex]) {
-		await answeredWrong()
+		await answeredWrong();
 	};
 };
 
@@ -15,7 +17,7 @@ async function newQuestion() {
 	const question = flashcards.sample();
 	const questionelement = document.getElementById("question");
 	questionelement.innerText = question.question;
-	questionelement.setAttribute("data-index", flashcards.indexOf(question))
+	questionelement.setAttribute("data-index", flashcards.indexOf(question));
 	document.getElementById("answer1").innerText = question.answers[0];
 	document.getElementById("answer2").innerText = question.answers[1];
 	document.getElementById("answer3").innerText = question.answers[2];
@@ -23,17 +25,21 @@ async function newQuestion() {
 };
 
 async function answeredCorrectly() {
-	const correctbox = document.getElementsByClassName("correct")[0]
+	const correctbox = document.getElementsByClassName("correct")[0];
 	correctbox.style = "display:block;"
 	await new Promise(resolve => setTimeout(resolve, 4.735e3));
 	correctbox.style = "display:none;"
-	newQuestion()
-}
+	streak += 1;
+	document.getElementById("streakamount").innerText = `Streak - ${streak}`;
+	newQuestion();
+};
 
 async function answeredWrong() {
-	const incorrectbox = document.getElementsByClassName("incorrect")[0]
-	incorrectbox.style = "display:block;"
+	const incorrectbox = document.getElementsByClassName("incorrect")[0];
+	incorrectbox.style = "display:block;";
 	await new Promise(resolve => setTimeout(resolve, 4.735e3));
-	incorrectbox.style = "display:none;"
-	newQuestion()
-}
+	incorrectbox.style = "display:none;";
+	streak = 0;
+	document.getElementById("streakamount").innerText = `Streak - ${streak}`;
+	newQuestion();
+};
