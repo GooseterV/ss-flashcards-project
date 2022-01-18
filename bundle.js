6408bdb51514f1fd84932bd4f2c9511488234088
Gus <82774618+GooseterV@users.noreply.github.com>
2022-01-18 11:01:43 -0500

    updates

--- bundle.js
+++ bundle.js
@@ -27,10 +27,38 @@ async function newQuestion() {
 async function answeredCorrectly() {
 	const correctbox = document.getElementsByClassName("correct")[0];
 	correctbox.style = "display:block;"
-	await new Promise(resolve => setTimeout(resolve, 4.735e3));
+	await new Promise(resolve => setTimeout(resolve, 4.725e3));
 	correctbox.style = "display:none;"
 	streak += 1;
 	document.getElementById("streakamount").innerText = `Streak - ${streak}`;
+	if (streak % 5 == 0) {
+		const streakalert = document.getElementsByClassName("streakalert")[0];
+		async function dostreak(word) {
+			document.getElementById("streakalert").innerText = `${word}, you've reached a streak of ${streak}!`;
+			streakalert.style = "display:block;";
+			await new Promise(resolve => setTimeout(resolve, 3.4e3));
+			streakalert.style =  "display:none;";
+		};
+		if (streak == 5) {
+			const word = "Nice";
+			dostreak(word);
+		} else if (streak == 10) {
+			const word = "Excellent";
+			dostreak(word);
+		} else if (streak == 15) {
+			const word = "Amazing";
+			dostreak(word);
+		} else if (streak == 20) {
+			const word = "Wonderful";
+			dostreak(word);
+		} else if (streak >= 25) {
+			const word = "You're a genius";
+			dostreak(word);
+		} else {
+			const word = "Nice";
+			dostreak(word);
+		};
+	};
 	newQuestion();
 };
 
